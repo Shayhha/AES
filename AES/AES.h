2caa876313e7c725140eba2f482b5edfc7e1e5f0
Shay Hahiashvili <shayhh@outlook.com>
2023-10-19 06:06:59 +0300

    Added function to print matrix of vectors as well as GaloisMult tables for MixColumns function for later use.

--- AES/AES.h
+++ AES/AES.h
@@ -14,13 +14,16 @@ class AES {
 private:
 	static const unsigned char SBOX[16][16]; //represent the SBOX table for AES
 	static const unsigned char INVSBOX[16][16]; //represent the inverse SBOX table for AES
+	static const unsigned char GaloisMult[15][256]; //represents the Galois multiplication tables for AES
 	static const int Nb = 4; //number of columns in the state (always 4 for AES)
 	static int Nk; //number of 32-bit words in the key 
-	static int Nr; //number of rounds (AES-128 has 10 rounds, AES-192 has 12 rounds, AES-256 has 14 rounds.)
+	static int Nr; //number of rounds (AES-128 has 10 rounds, AES-192 has 12 rounds, AES-256 has 14 rounds)
+	static const int BlockSize = Nb * Nb; //represents the size of AES block that is always 16 bytes (128-bit)
 
 public:
 	static const vector<vector<unsigned char>> KeySchedule(const vector<unsigned char>& key, const bool isDecrypt=false);
 	static void SetOperationMode(const int blockSize);
+	static void PrintMatrix(const vector<vector<unsigned char>>& matrix);
 	static vector<unsigned char> RotWord(const vector<unsigned char>& word);
 	static vector<unsigned char> SubWord(const vector<unsigned char>& word);
 	static const unsigned char Rcon(const unsigned char& value);

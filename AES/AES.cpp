1cfc80aa9812460da2d49b401f50b2ec8fe0cf7a
Shay Hahiashvili <shayhh@outlook.com>
2023-11-01 20:27:52 +0200

    Added comments.

--- AES/AES.cpp
+++ AES/AES.cpp
@@ -867,7 +867,8 @@ int main() {
         cout << str << endl;
     }
     catch (const runtime_error& e) {
-        cout << e.what() << endl;
+        cerr << e.what() << endl;
+        return 1;
     }
 
     return 0;
